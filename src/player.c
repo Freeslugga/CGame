e35d0ec5fb6c0fed7fd9ad2a15f6b923c7ec70e7
Freeslugga <jjf1019@gmail.com>
2025-03-25 21:31:30 -0400

    Fix the null-termination to a char rather than a string

--- src/player.c
+++ src/player.c
@@ -5,7 +5,7 @@
 void init_player(Player *player, const char *name)
 {
     strncpy(player->name, name, NAME_LEN -1);
-    player->name[NAME_LEN -1] = "\0";
+    player->name[NAME_LEN -1] = '\0';
     player->health = 100;
     player->money = 100.0f;
 }
